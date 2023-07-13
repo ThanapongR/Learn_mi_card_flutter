4b03907faa8ee91ecfb94b9f85feb61ca55c98b2
thanapongr <thanapong.rodruang@gmail.com>
2023-07-13 19:06:48 +0700

    Add my profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,93 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 72.0,
+                backgroundImage: AssetImage('images/thanapong.jpg'),
+              ),
+              Text(
+                'Thanapong R',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 48.0,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.white,
+                  fontSize: 24.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 36.0,
+                width: 240.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 16.0,
+                  horizontal: 24.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    'Thanapong.Rodruang@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      color: Colors.black,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 16.0,
+                  horizontal: 24.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.account_circle,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    'linkedin.com/in/thanapong-rodruang',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      color: Colors.black,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
